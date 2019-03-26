d28d196bdbf42512f27e895a51f9e91a7b2bf79c
Mark Barr <mbarr@veobot.com>
2019-03-25 21:45:12 -0400

    eslint: updated .eslintrc.js with expanded rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,116 +1,158 @@
+'use strict';
+
 module.exports = {
-  "env": {
-    "node": true,
-    "es6": true,
-    "mocha": true
+  'parser': 'espree',
+  'parserOptions': { 'sourceType': 'script' },
+  'env': {
+    'mocha': true,
+    'node': true,
+    'es6': true
   },
-  "globals": {
-    "retry": true
-  },
-  "rules": {
-    // Basic rules
-    "no-empty": ["error", {                               // Don't allow empty block statements...
-      "allowEmptyCatch": true                             // except for empty catch statements
-    }],
-    "indent": ["error", 2, {                              // Two spaces per indentation, no tabs allowed (fixable)
-      "SwitchCase": 1                                     // Overrides default indentation enforcement for switch cases.
-                                                          // Note integer represents number of indentation units.
-                                                          // Ergo, 2 spaces/indentation * 1 indentation = 2 spaces
-    }],
-    "comma-spacing": "error",                             // Enforces `a, b`. Will reject `a,b` (fixable)
-    "semi": "error",                                      // Enforces semicolons after statements (fixable)
-    "space-infix-ops": "error",                           // Enforces spaces around operators (fixable)
-    "space-before-blocks": "error",                       // Enforces spaces before { (fixable)
-    "no-trailing-spaces": "error",                        // Enforces trailing whitespace (fixable)
-    "comma-dangle": ["error", "never"],                   // No trailing commmas (fixable)
-    "no-multi-spaces": "error",                           // One space per gap (fixable)
-    "eol-last": "error",                                  // Mandate newlines at the end of files (fixable)
-    "quotes": ["error", "single", {                        // Mandate single quotes (fixable)
-      "avoidEscape": true
-    }],
-    "no-multiple-empty-lines": ["error", {
-      "max": 1
-    }],                                                   // No overuse of newlines (fixable)
-    "eqeqeq": ["error", "always"],                        // Never use != or == (fixable)
-    "key-spacing": ["error", {                            // Enforces spaces after colons in object literals
-      "beforeColon": false,                               // i.e. `{a: b}` is valid `{a : b}` or `{a:b}` not valid
-      "afterColon": true
-    }],
-    "no-dupe-args": "error",                              // Don't allow accidental duplication of arguments
-    "no-dupe-keys": "error",                              // Don't allow duplication of keys in object literals
-    "no-debugger": "error",                               // Don't allow submissions which retain debugger statements
-    "no-caller": "error",                                 // No access of `caller` and `callee` attributes. Performance killers
-    "no-undef": "error",                                  // Don't allow access of implicit globals
-                                                          // (see above list of accepted globals)
-    "keyword-spacing": "error",                           // Accept `if () {}`, reject `if() {}`
-    "no-unreachable": "error",
-    "no-cond-assign": ["error", "except-parens"],         // No assignments inside conditionals
-    "no-sparse-arrays": "error",                          // No empty spaces in array literals
-    "no-eval": "error",                                   // No using `eval` statements
-    "no-loop-func": "error",                              // No hardcoding external loop iterators into functions
-    "no-proto": "error",                                  // No using `__proto__` at all
-    "no-with": "error",                                   // No using the `with` keyword
-    "brace-style": ["error", "1tbs", {                    // Enforce 1tbs brace style (fixable)
-      "allowSingleLine": true
-    }],
-    // Styling
-    "array-bracket-spacing": [ // enforce spaces inside of brackets
-      "error",
-      "always"
-    ],
-    "dot-location": [ // enforce consistent newlines before and after dots
-      "error",
-      "object"
-    ],
-    "dot-notation": "error", // enforce dot notation whenever possible
-    "no-const-assign": "error", // disallow reassigning const variables
-    "no-constant-condition": "error", // disallow constant expressions in conditions
-    "no-empty-character-class": "error", // disallow empty character classes in regular expressions
-    "no-floating-decimal": "error", // disallow leading or trailing decimal points in numeric literals
-    "no-mixed-requires": "error", // disallow require calls to be mixed with regular variable declarations
-    "no-redeclare": "error", // disallow variable redeclaration
-    "no-shadow": [ // disallow variable declarations from shadowing variables declared in the outer scope (no-shadow)
-      "error",
-      {
-        "allow": [
-          "resolve",
-          "reject",
-          "error",
-          "response",
-          "body"
-        ]
-      }
-    ],
-    "no-unmodified-loop-condition": "error", // disallow unmodified loop conditions
-    "no-unused-expressions": "error", // disallow unused expressions
-    "no-unused-vars": "error", // disallow unused variables
-    "no-use-before-define": [ // disallow the use of variables before they are defined
-      "error",
-      "nofunc"
-    ],
-    "no-useless-call": "error", // disallow unnecessary calls to .call() and .apply()
-    "no-useless-escape": "error", // disallow unnecessary escape characters
-    "no-useless-return": "error", // disallow redundant return statements
-    "no-var": "error", // require let or const instead of var
-    "object-curly-newline": [ // enforce consistent line breaks inside braces
-      "error",
-      {
-        "minProperties" : 1
-      }
-    ],
-    "object-curly-spacing": [ // enforce consistent spacing inside braces
-      "error",
-      "always"
-    ],
-    "object-property-newline": "error", // enforce placing object properties on separate lines
-    "one-var": [ // enforce variables to be declared separately in functions
-      "error",
-      "never"
-    ],
-    "one-var-declaration-per-line": "error", // require newlines around variable declarations
-    "prefer-const": "error", // require const declarations for variables that are never reassigned after declared
-    "prefer-promise-reject-errors": "error", // require using Error objects as Promise rejection reasons
-    "strict": "error", // require strict mode directives
-    "yoda": "error" // disallow Yoda conditions
+  'globals': {},
+  'rules': {
+    'accessor-pairs': 'error',
+    'array-bracket-spacing': [ 'error', 'always' ],
+    'array-callback-return': 'error',
+    'arrow-body-style': [ 'error', 'always' ],
+    'arrow-spacing': 'error',
+    'block-spacing': [ 'error', 'always' ],
+    'brace-style': [ 'error', '1tbs', { 'allowSingleLine': true } ],
+    'callback-return': 'error',
+    'camelcase': [ 'error', { 'properties': 'always' } ],
+    'comma-dangle': [ 'error', 'never' ],
+    'comma-spacing': 'error',
+    'comma-style': [ 'error', 'last' ],
+    'complexity': [ 'error', 25 ],
+    'consistent-return': 'error',
+    'constructor-super': 'error',
+    'default-case': 'error',
+    'dot-location': [ 'error', 'object' ],
+    'dot-notation': 'error',
+    'eol-last': 'error',
+    'eqeqeq': [ 'error', 'always' ],
+    'handle-callback-err': 'error',
+    'indent': [ 'error', 2, { 'SwitchCase': 1 } ],
+    'key-spacing': [ 'error', {
+      'beforeColon': false,
+      'afterColon': true
+    } ],
+    'keyword-spacing': 'error',
+    'max-len': [ 'error', 120, 4 ],
+    'max-params': [ 'error', 4 ],
+    'new-cap': 'error',
+    'newline-per-chained-call': 'error',
+    'no-caller': 'error',
+    'no-case-declarations': 'error',
+    'no-class-assign': 'error',
+    'no-cond-assign': [ 'error', 'except-parens' ],
+    'no-confusing-arrow': 'error',
+    'no-const-assign': 'error',
+    'no-constant-condition': 'error',
+    'no-debugger': 'error',
+    'no-delete-var': 'error',
+    'no-dupe-args': 'error',
+    'no-dupe-class-members': 'error',
+    'no-dupe-keys': 'error',
+    'no-else-return': 'error',
+    'no-empty': [ 'error', { 'allowEmptyCatch': true } ],
+    'no-empty-character-class': 'error',
+    'no-empty-pattern': 'error',
+    'no-eq-null': 'error',
+    'no-eval': 'error',
+    'no-ex-assign': 'error',
+    'no-extra-bind': 'error',
+    'no-extra-boolean-cast': 'error',
+    'no-extra-parens': 'error',
+    'no-extra-semi': 'error',
+    'no-fallthrough': 'error',
+    'no-floating-decimal': 'error',
+    'no-implicit-coercion': 'error',
+    'no-implicit-globals': 'error',
+    'no-implied-eval': 'error',
+    'no-invalid-regexp': 'error',
+    'no-invalid-this': 'error',
+    'no-irregular-whitespace': 'error',
+    'no-labels': 'error',
+    'no-lone-blocks': 'error',
+    'no-lonely-if': 'error',
+    'no-mixed-requires': 'error',
+    'no-multi-spaces': 'error',
+    'no-multi-str': 'error',
+    'no-multiple-empty-lines': [ 'error', { 'max': 1 } ],
+    'no-native-reassign': [ 'error', { 'exceptions': [ 'Object' ] } ],
+    'no-negated-in-lhs': 'error',
+    'no-new': 'error',
+    'no-new-func': 'error',
+    'no-new-require': 'error',
+    'no-new-symbol': 'error',
+    'no-new-wrappers': 'error',
+    'no-obj-calls': 'error',
+    'no-octal-escape': 'error',
+    'no-path-concat': 'error',
+    'no-proto': 'error',
+    'no-redeclare': 'error',
+    'no-regex-spaces': 'error',
+    'no-return-assign': 'error',
+    'no-self-assign': 'error',
+    'no-self-compare': 'error',
+    'no-sequences': 'error',
+    'no-shadow': [ 'error', { 'allow': [ 'body', 'error', 'reject', 'resolve', 'response', 'result' ] } ],
+    'no-shadow-restricted-names': 'error',
+    'no-spaced-func': 'error',
+    'no-sparse-arrays': 'error',
+    'no-this-before-super': 'error',
+    'no-throw-literal': 'error',
+    'no-trailing-spaces': 'error',
+    'no-undef': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unmodified-loop-condition': 'error',
+    'no-unneeded-ternary': 'error',
+    'no-unreachable': 'error',
+    'no-unused-expressions': 'error',
+    'no-unused-vars': 'error',
+    'no-use-before-define': [ 'error', 'nofunc' ],
+    'no-useless-call': 'error',
+    'no-useless-concat': 'error',
+    'no-useless-constructor': 'error',
+    'no-useless-escape': 'error',
+    'no-useless-return': 'error',
+    'no-var': 'error',
+    'no-void': 'error',
+    'no-warning-comments': [ 'error', {
+      'terms': [ 'todo', 'fixme', 'hack' ],
+      'location': 'anywhere'
+    } ],
+    'no-with': 'error',
+    'object-curly-newline': [ 'error', { 'minProperties': 2 } ],
+    'object-curly-spacing': [ 'error', 'always' ],
+    'object-property-newline': 'error',
+    'object-shorthand': 'error',
+    'one-var': [ 'error', 'never' ],
+    'one-var-declaration-per-line': 'error',
+    'padded-blocks': [ 'error', 'never' ],
+    'prefer-arrow-callback': 'error',
+    'prefer-const': 'error',
+    'prefer-promise-reject-errors': 'error',
+    'prefer-rest-params': 'error',
+    'prefer-spread': 'error',
+    'prefer-template': 'error',
+    'quotes': [ 'error', 'single', { 'avoidEscape': true } ],
+    'radix': [ 'error', 'always' ],
+    'require-yield': 'error',
+    'semi': 'error',
+    'space-before-blocks': 'error',
+    'space-infix-ops': 'error',
+    'strict': 'error',
+    'template-curly-spacing': [ 'error', 'always' ],
+    'use-isnan': 'error',
+    'valid-typeof': 'error',
+    'yield-star-spacing': [ 'error', 'before' ],
+    'yoda': 'error'
+    // 'no-div-regex': 'error',
+    // 'no-magic-numbers': [ 'error', { 'ignoreArrayIndexes': true, 'ignore':  [ 0, 1 ] } ],
+    // 'no-native-reassign': [ 'error', { 'exceptions': [ 'Object' ] } ],
+    // 'no-sync': 'error',
+    // 'no-underscore-dangle': 'error',
+    // 'prefer-reflect': 'error',
   }
 };
