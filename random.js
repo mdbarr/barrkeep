0acea8711f04eed49f1767d0283482354287cbbb
Mark Barr <mark@hyperingenuity.com>
2019-03-26 13:05:09 -0400

    v2.0: Perlin Noise generator added to random

--- random.js
+++ random.js
@@ -4,6 +4,9 @@ Math.$random = function(min = 0, max = 10) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+//////////
+// Mersenne Twister - pseudo random number generator
+
 function MersenneTwister(seed) {
   seed = seed || Date.now();
   this.N = 624;
@@ -66,4 +69,97 @@ MersenneTwister.prototype.random = function(min, max) {
   return (y + 0.5) / this.I;
 };
 
-module.exports = MersenneTwister;
+//////////
+// Perlin Noise
+
+function interpolate(x0, x1, alpha) {
+  return x0 * (1 - alpha) + alpha * x1;
+}
+
+function generateWhiteNoise(width, height, prng) {
+  const noise = new Array(width * height);
+  for (let i = 0; i < noise.length; ++i) {
+    if (prng) {
+      noise[i] = prng.random();
+    } else {
+      noise[i] = Math.random();
+    }
+  }
+  return noise;
+}
+
+function generatePerlinNoise(width, height, options) {
+  options = options || {};
+
+  let prng = null;
+  if (options.prng) {
+    prng = options.prng;
+  } else if (options.seed) {
+    prng = new MersenneTwister(options.seed);
+  }
+
+  const octaveCount = options.octaveCount || 4;
+  const persistence = options.persistence || 0.2;
+  const whiteNoise = generateWhiteNoise(width, height, prng);
+
+  let amplitude = options.amplitude || 0.1;
+
+  function generateSmoothNoise(octave) {
+    const noise = new Array(width * height);
+    const samplePeriod = Math.pow(2, octave);
+    const sampleFrequency = 1 / samplePeriod;
+    let noiseIndex = 0;
+    for (let y = 0; y < height; ++y) {
+      const sampleY0 = Math.floor(y / samplePeriod) * samplePeriod;
+      const sampleY1 = (sampleY0 + samplePeriod) % height;
+      const vertBlend = (y - sampleY0) * sampleFrequency;
+      for (let x = 0; x < width; ++x) {
+        const sampleX0 = Math.floor(x / samplePeriod) * samplePeriod;
+        const sampleX1 = (sampleX0 + samplePeriod) % width;
+        const horizBlend = (x - sampleX0) * sampleFrequency;
+
+        const top = interpolate(whiteNoise[sampleY0 * width + sampleX0],
+          whiteNoise[sampleY1 * width + sampleX0],
+          vertBlend);
+
+        const bottom = interpolate(whiteNoise[sampleY0 * width + sampleX1],
+          whiteNoise[sampleY1 * width + sampleX1],
+          vertBlend);
+
+        noise[noiseIndex] = interpolate(top, bottom, horizBlend);
+        noiseIndex += 1;
+      }
+    }
+    return noise;
+  }
+
+  const smoothNoiseList = new Array(octaveCount);
+  for (let i = 0; i < octaveCount; ++i) {
+    smoothNoiseList[i] = generateSmoothNoise(i);
+  }
+  const perlinNoise = new Array(width * height);
+  let totalAmplitude = 0;
+
+  for (let i = octaveCount - 1; i >= 0; --i) {
+    amplitude *= persistence;
+    totalAmplitude += amplitude;
+
+    for (let j = 0; j < perlinNoise.length; ++j) {
+      perlinNoise[j] = perlinNoise[j] || 0;
+      perlinNoise[j] += smoothNoiseList[i][j] * amplitude;
+    }
+  }
+
+  for (let i = 0; i < perlinNoise.length; ++i) {
+    perlinNoise[i] /= totalAmplitude;
+  }
+
+  return perlinNoise;
+}
+
+//////////
+
+module.exports = {
+  MersenneTwister,
+  generatePerlinNoise
+};
