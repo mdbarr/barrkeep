4e69b08c83d6f660c3d6db6bab0565425ab4f4ee
Mark Barr <mark@hyperingenuity.com>
2019-07-06 14:02:31 -0400

    expect: many additional expectations

--- expect.js
+++ expect.js
@@ -75,6 +75,24 @@ Expectation.prototype.undefined = function() {
   return this;
 };
 
+Expectation.prototype.finite = function() {
+  assert.strictEqual(Number.isFinite(this.value), true);
+  return this;
+};
+
+Expectation.prototype.infinite = function() {
+  assert.strictEqual(this.value === Infinity, true);
+  return this;
+};
+Expectation.prototype.infinity = Expectation.prototype.infinite;
+Expectation.prototype.Infinity = Expectation.prototype.infinite;
+
+Expectation.prototype.nan = function() {
+  assert.strictEqual(Number.isNaN(this.value), true);
+  return this;
+};
+Expectation.prototype.NaN = Expectation.prototype.nan;
+
 Expectation.prototype.instanceof = function(constructor) {
   assert.strictEqual(this.value instanceof constructor, true);
   return this;
@@ -88,12 +106,130 @@ Expectation.prototype.typeof = function(type) {
 Expectation.prototype.typeOf = Expectation.prototype.typeof;
 Expectation.prototype.type = Expectation.prototype.typeof;
 
+Expectation.prototype.Array = function() {
+  assert.strictEqual(Array.isArray(this.value), true);
+  return this;
+};
+
+Expectation.prototype.Boolean = function() {
+  assert.strictEqual(this.value instanceof Boolean || typeof this.value === 'boolean', true);
+  return this;
+};
+
+Expectation.prototype.Date = function() {
+  assert.strictEqual(this.value instanceof Date, true);
+  return this;
+};
+
+Expectation.prototype.Error = function() {
+  assert.strictEqual(this.value instanceof Error, true);
+  return this;
+};
+
+Expectation.prototype.Function = function() {
+  assert.strictEqual(this.value instanceof Function || typeof this.value === 'function', true);
+  return this;
+};
+
+Expectation.prototype.Map = function() {
+  assert.strictEqual(this.value instanceof Map, true);
+  return this;
+};
+
+Expectation.prototype.Number = function() {
+  assert.strictEqual(this.value instanceof Number || typeof this.value === 'number', true);
+  return this;
+};
+
+Expectation.prototype.Object = function() {
+  assert.strictEqual(this.value instanceof Object || typeof this.value === 'object', true);
+  return this;
+};
+
+Expectation.prototype.Promise = function() {
+  assert.strictEqual(this.value instanceof Promise, true);
+  return this;
+};
+
+Expectation.prototype.RegExp = function() {
+  assert.strictEqual(this.value instanceof RegExp, true);
+  return this;
+};
+
+Expectation.prototype.Set = function() {
+  assert.strictEqual(this.value instanceof Set, true);
+  return this;
+};
+
+Expectation.prototype.String = function() {
+  assert.strictEqual(this.value instanceof String || typeof this.value === 'string', true);
+  return this;
+};
+
+Expectation.prototype.Symbol = function() {
+  assert.strictEqual(this.value instanceof Symbol, true);
+  return this;
+};
+
+Expectation.prototype.WeakMap = function() {
+  assert.strictEqual(this.value instanceof WeakMap, true);
+  return this;
+};
+
+Expectation.prototype.WeakSet = function() {
+  assert.strictEqual(this.value instanceof WeakSet, true);
+  return this;
+};
+
+Expectation.prototype.has = function(value) {
+  assert.strictEqual(this.value instanceof Map || this.value instanceof Set ||
+                     this.value instanceof WeakMap || this.value instanceof WeakSet, true);
+  assert.strictEqual(this.value.has(value), true);
+  return this;
+};
+
 Expectation.prototype.includes = function(value) {
   assert.strictEqual(typeof this.value === 'string' || Array.isArray(this.value), true);
   assert.strictEqual(this.value.includes(value), true);
   return this;
 };
 Expectation.prototype.include = Expectation.prototype.includes;
+
+Expectation.prototype.in = function(...args) {
+  const array = args.length === 1 ? args[0] : args;
+  assert.strictEqual(array.includes(this.value), true);
+  return this;
+};
+
+Expectation.prototype.within = function(lower, upper, inclusive = true) {
+  if (inclusive) {
+    assert.strictEqual(this.value >= lower, true);
+    assert.strictEqual(this.value <= upper, true);
+  } else {
+    assert.strictEqual(this.value > lower, true);
+    assert.strictEqual(this.value < upper, true);
+  }
+  return this;
+};
+
+Expectation.prototype.approximately = function(value, tolerance = 0.00005) {
+  assert.strictEqual(Math.abs(value - this.value) <= tolerance, true);
+  return this;
+};
+
+Expectation.prototype.startWith = function(string) {
+  assert.strictEqual(typeof this.value === 'string', true);
+  assert.strictEqual(this.value.startsWith(string), true);
+  return this;
+};
+Expectation.prototype.startsWith = Expectation.prototype.startWith;
+
+Expectation.prototype.endWith = function(string) {
+  assert.strictEqual(typeof this.value === 'string', true);
+  assert.strictEqual(this.value.endsWith(string), true);
+  return this;
+};
+Expectation.prototype.endsWith = Expectation.prototype.endWith;
 
 //////////
 
