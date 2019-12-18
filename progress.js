e97d66b2a3b86687e9d201743b65a0d19f55e3d3
Mark Barr <mark@hyperingenuity.com>
2019-12-18 16:46:19 -0500

    progress: cleaner interface using getters and setters

--- progress.js
+++ progress.js
@@ -7,13 +7,14 @@ const {
 const spinners = require('./spinners.json');
 
 class ProgressBar {
-  constructor({
-    total = 10, format = '[$progress]', stream = process.stderr, width,
-    complete = '=', incomplete = ' ', head = '>', clear, y,
+  constructor ({
+    total = 10, value = 0, format = '[$progress]', stream = process.stderr,
+    y, x = 0, width, complete = '=', incomplete = ' ', head = '>', clear,
     interval, environment = {}, spinner = 'dots'
   } = {}) {
-    this.total = total;
-    this.width = width || Math.max(this.total, 60);
+    this._total = total;
+    this._value = value;
+    this.width = width || Math.max(this._total, 60);
 
     this.stream = stream;
     this.format = format;
@@ -25,6 +26,7 @@ class ProgressBar {
 
     this.clear = clear === undefined ? false : clear;
     this.y = y || undefined;
+    this.x = x;
 
     spinner = spinners[spinner] ? spinner : 'dots';
 
@@ -34,8 +36,6 @@ class ProgressBar {
     this.environment = environment;
     this.initialEnvironment = Object.assign({}, environment);
 
-    this.value = 0;
-
     this.complete = false;
     this.lastUpdate = false;
 
@@ -44,7 +44,7 @@ class ProgressBar {
     this.tick = this.start;
     this.eta = 0;
 
-    if (this.total === 0) {
+    if (this._total === 0) {
       this.complete = true;
     } else {
       this.render();
@@ -61,27 +61,36 @@ class ProgressBar {
     }
   }
 
-  progress (increment = 1) {
+  get value () {
+    return this._value;
+  }
+
+  set value (value) {
     if (this.complete) {
       return;
     }
 
-    if (increment > 0) {
-      this.value += increment;
-    } else if (increment < 0) {
-      this.total += increment;
-    }
+    this._value = value;
 
     this.tick = timestamp();
     const elapsed = this.tick - this.start;
-    this.eta = elapsed / this.value * (this.total - this.value);
+    this.eta = elapsed / this._value * (this._total - this._value);
 
     this.render();
 
-    if (this.value >= this.total) {
+    if (this._value >= this._total) {
       this.complete = true;
       this.done();
     }
+  }
+
+  get total () {
+    return this._total;
+  }
+
+  set total (value) {
+    this._total = value;
+    this.render();
   }
 
   render () {
@@ -89,22 +98,22 @@ class ProgressBar {
       return;
     }
 
-    let ratio = this.value / this.total;
+    let ratio = this._value / this._total;
     ratio = Math.min(Math.max(ratio, 0), 1);
 
     const percent = Math.floor(ratio * 100);
 
     const now = timestamp();
     const elapsed = now - this.start;
-    const eta = this.value === 0 ? 0 : this.eta - (now - this.tick);
-    const rate = this.value / (elapsed / 1000);
+    const eta = this._value === 0 ? 0 : this.eta - (now - this.tick);
+    const rate = this._value / (elapsed / 1000);
 
     const spinner = this.spinner[this.ticks];
 
     let string = this.format.
-      replace(/\$value/g, formatNumber(this.value)).
-      replace(/\$total/g, formatNumber(this.total)).
-      replace(/\$remaining/g, formatNumber(this.total - this.value)).
+      replace(/\$_value/g, formatNumber(this._value)).
+      replace(/\$_total/g, formatNumber(this._total)).
+      replace(/\$remaining/g, formatNumber(this._total - this._value)).
       replace(/\$elapsed/g, duration(elapsed)).
       replace(/\$eta/g, isNaN(eta) || !isFinite(eta) || !eta ? 'unknown' : duration(eta)).
       replace(/\$percent/g, `${ percent.toFixed(0) }%`).
@@ -125,7 +134,7 @@ class ProgressBar {
 
     let completeLength = Math.max(0, Math.round(width * ratio));
     let headLength = 0;
-    if (completeLength < this.total && completeLength > 0) {
+    if (completeLength < this._total && completeLength > 0) {
       completeLength--;
       headLength = 1;
     }
@@ -139,7 +148,7 @@ class ProgressBar {
       replace(/\$progress/g, '');
 
     if (this.lastUpdate !== string) {
-      this.stream.cursorTo(0, this.y);
+      this.stream.cursorTo(this.x, this.y);
       this.stream.write(`\x1b[?25l${ string }`);
       this.stream.clearLine(1);
       this.lastUpdate = string;
@@ -154,7 +163,7 @@ class ProgressBar {
     if (this.clear) {
       if (this.stream.clearLine) {
         this.stream.clearLine();
-        this.stream.cursorTo(0, this.y);
+        this.stream.cursorTo(this.x, this.y);
       }
     } else {
       this.stream.write('\n');
@@ -162,7 +171,7 @@ class ProgressBar {
   }
 
   reset () {
-    this.value = 0;
+    this._value = 0;
     this.complete = 0;
     this.start = timestamp();
     Object.assign(this.environment, this.initialEnvironment);
@@ -229,7 +238,7 @@ class Spinner {
     }
   }
 
-  stop() {
+  stop () {
     clearInterval(this.update);
 
     if (this.clear) {
