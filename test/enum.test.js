81870c0f837d3379df3310f6e43a212241967360
Mark Barr <mark@hyperingenuity.com>
2020-05-27 23:11:51 -0400

    enum: simple test

---
+++ test/enum.test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const Enum = require('../classes/Enum');
+
+describe('Enum Test', () => {
+  let Foo;
+
+  it('should create a new enum', () => {
+    Foo = new Enum('a', 'b', 'c');
+    expect(Foo.A).toBe(0);
+    expect(Foo.a).toBe(0);
+    expect(Foo.B).toBe(1);
+    expect(Foo.c).toBe(2);
+    expect(Foo.D).toBe(undefined);
+  });
+
+  it('should create an enum variable', () => {
+    const foo = Enum.A;
+    expect(foo).toBe(Enum.A);
+  });
+});
